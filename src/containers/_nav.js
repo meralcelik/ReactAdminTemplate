fb8e9917543e9f7257decc835f562743629197ee
meralcelik <meralcelik1444@gmail.com>
2021-06-22 11:35:26 +0300

    denemeh

---
+++ src/containers/_nav.js
@@ -0,0 +1,324 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+
+const _nav =  [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Ana Sayfa',
+    to: '/dashboard',
+    icon: <CIcon name="cil-home" customClasses="c-sidebar-nav-icon"/>,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    }
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Theme']
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Colors',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Typography',
+    to: '/theme/typography',
+    icon: 'cil-pencil',
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Components']
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Belgeler',
+    route: '/base',
+    icon: 'cil-file',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gelen Belgeler',
+        icon: 'cil-task',
+        to: '/base/breadcrumbs',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gönderilen Belgeler',
+        icon: 'cil-share-boxed',
+
+        to: '/base/cards',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Carousel',
+        to: '/base/carousels',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Collapse',
+        to: '/base/collapses',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Forms',
+        to: '/base/forms',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Jumbotron',
+        to: '/base/jumbotrons',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'List group',
+        to: '/base/list-groups',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Navs',
+        to: '/base/navs',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Navbars',
+        to: '/base/navbars',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Pagination',
+        to: '/base/paginations',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Popovers',
+        to: '/base/popovers',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Progress',
+        to: '/base/progress-bar',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Switches',
+        to: '/base/switches',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Tables',
+        to: '/base/tables',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Tabs',
+        to: '/base/tabs',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Tooltips',
+        to: '/base/tooltips',
+      },
+    ],
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Anket',
+    route: '/buttons',
+    icon: 'cil-chart-pie',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Anket Oluştur',
+        icon: 'cil-pencil',
+        to: '/buttons/buttons',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Anket Analizleri',
+        icon: 'cil-av-timer',
+        to: '/buttons/brand-buttons',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Buttons groups',
+        to: '/buttons/button-groups',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Dropdowns',
+        to: '/buttons/button-dropdowns',
+      }
+    ],
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Charts',
+    to: '/charts',
+    icon: 'cil-chart-pie'
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Ödev',
+    route: '/icons',
+    icon: 'cil-align-left',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Ödev Oluştur',
+        icon: 'cil-pencil',
+        to: '/icons/coreui-icons',
+
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gelen Ödevler',
+        icon: 'cil-notes',
+        to: '/icons/flags',
+        badge: {
+          color: 'success',
+          text: 'NEW',
+        },
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'CoreUI Brands',
+        to: '/icons/brands',
+      },
+    ],
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Mesajlar',
+    route: '/notifications',
+    icon: 'cil-speech',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Mesaj Oluştur',
+        to: '/notifications/alerts',
+        icon: 'cil-pencil',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gelen Mesajlar',
+        icon: 'cil-envelope-letter',
+        to: '/notifications/badges',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Gönderilen Mesajlar',
+        to: '/notifications/modals',
+        icon: 'cil-cursor',
+
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Toaster',
+        to: '/notifications/toaster'
+      }
+    ]
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Şikayetler',
+    to: '/widgets',
+    icon: 'cil-warning',
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
+    _tag: 'CSidebarNavDivider'
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Extras'],
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Pages',
+    route: '/pages',
+    icon: 'cil-star',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Login',
+        to: '/login',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Register',
+        to: '/register',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Error 404',
+        to: '/404',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Error 500',
+        to: '/500',
+      },
+    ],
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Disabled',
+    icon: 'cil-ban',
+    badge: {
+      color: 'secondary',
+      text: 'NEW',
+    },
+    addLinkClass: 'c-disabled',
+    'disabled': true
+  },
+  {
+    _tag: 'CSidebarNavDivider',
+    className: 'm-2'
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Labels']
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Label danger',
+    to: '',
+    icon: {
+      name: 'cil-star',
+      className: 'text-danger'
+    },
+    label: true
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Label info',
+    to: '',
+    icon: {
+      name: 'cil-star',
+      className: 'text-info'
+    },
+    label: true
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Label warning',
+    to: '',
+    icon: {
+      name: 'cil-star',
+      className: 'text-warning'
+    },
+    label: true
+  },
+  {
+    _tag: 'CSidebarNavDivider',
+    className: 'm-2'
+  }
+]
+
+export default _nav
